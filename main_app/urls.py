2391b6f254c09f87f3576734e8283d6b831d0869
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-20 12:36:52 -0700

    add connection to database and lizards detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'), 
-    path('lizards/', views.lizards_index, name='index')
+    path('lizards/', views.lizards_index, name='index'),
+    path('lizards/<int:lizard_id>/', views.lizards_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Lizard
 
 # Create your views here.
 def home(request):
@@ -8,12 +9,13 @@ def about(request):
     return render(request, 'about.html')
 
 def lizards_index(request):
+    lizards = Lizard.objects.all()
     return render(request, 'lizards/index.html', {
         'lizards': lizards
     })
 
-lizards = [
-    {'name': 'DJ Crazy Tongue', 'species': 'Blue-tongued skink', 'description': 'clever and hungry', 'age': 1},
-    {'name': 'Lizzie', 'species': 'Mexican mole lizard', 'description': 'the sweetest girl', 'age': 3},
-    {'name': 'Kurt', 'species': 'Komodo dragon', 'description': 'large and in charge', 'age': 15},
-]
+def lizards_detail(request, lizard_id):
+    lizard = Lizard.objects.get(id=lizard_id)
+    return render(request, 'lizards/detail.html', {
+        'lizard': lizard
+    })
