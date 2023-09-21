018ecf06b89d1f5ad64c68fced7a7374040100f9
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-21 13:32:19 -0700

    add bonus, display a message with status of hungry or fed

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 
@@ -21,6 +22,9 @@ class Lizard(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'lizard_id': self.id})
     
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
 class Feeding(models.Model):
     date = models.DateField('feeding date')
     meal = models.CharField(
@@ -32,3 +36,6 @@ class Feeding(models.Model):
 
     def __str__(self):
         return f'{self.get_meal_display()} on {self.date}'
+    
+    class Meta: 
+        ordering = ['-date']
