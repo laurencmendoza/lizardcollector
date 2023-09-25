01081c3d997e41548f669f00fe6072442c763ca1
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-25 12:55:42 -0700

    prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lizardcollector.wsgi
--- lizardcollector/settings.py
+++ lizardcollector/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Other settings above
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
